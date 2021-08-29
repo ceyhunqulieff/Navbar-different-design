7dda4ee997adaece62682337cf67ce0bc24a6f96
ceyhunqulieff <88338528+ceyhunqulieff@users.noreply.github.com>
2021-08-29 20:30:11 +0400

    test

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+const pullDown = document.getElementById('pullDown');
+// const menu = document.getElementById('menu');
+const alert = document.querySelector('.alert');
+const menuArray = document.querySelectorAll('.menu > *');
+// const menuCategory = document.querySelectorAll('.menu > ul.items > li');
+pullDown.oninput = function () {
+  menu.style.width = pullDown.value + '%';
+  if (pullDown.value == 100) {
+    alert.innerText = 'Qoruyucu açıldı!';
+    alert.style.display = 'block';
+    setTimeout(() => {
+      alert.style.display = 'none';
+    }, 3000);
+  } else if (pullDown.value >= 70) {
+    menuArray.forEach((element) => {
+      element.style.display = 'inline-block';
+    });
+  } else if (pullDown.value == 0) {
+    alert.innerText = 'Qoruyucu bağlandı!';
+    alert.style.display = 'block';
+    setTimeout(() => {
+      alert.style.display = 'none';
+    }, 3000);
+  } else {
+    menuArray.forEach((element) => {
+      element.style.display = 'none';
+    });
+  }
+};
